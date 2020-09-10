ca417342f04a19ab49489ad718aaeb09e4e4aba9
divine <48183131+divine@users.noreply.github.com>
2020-09-10 14:00:19 +0300

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,126 @@
+export default {
+  /*
+   ** Rendering mode
+   ** Doc: https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+
+    /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
+  modules: [
+    // Doc: https://http.nuxtjs.org
+    "@nuxt/http",
+    // TODO: Remove it if you want to eject from codeSandbox
+    "./codesandbox",
+    "nuxt-i18n"
+  ],
+
+  i18n: {
+    baseUrl: "",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieDomain: null,
+      cookieKey: "i18n_redirected",
+      alwaysRedirect: true,
+      fallbackLocale: "",
+      onlyOnRoot: true
+    },
+    differentDomains: false,
+    strategy: "prefix",
+    locales: [
+      {
+        code: "en",
+        iso: "en",
+        name: "English"
+      },
+      {
+        code: "fr",
+        iso: "fr-FR",
+        name: "Français"
+      }
+    ],
+    defaultLocale: "en",
+    langDir: null,
+    lazy: false,
+    parsePages: true,
+    pages: {},
+    rootRedirect: null,
+    seo: false,
+    vueI18n: {
+      messages: {
+        fr: {
+          home: "Accueil",
+          about: "À propos"
+        },
+        en: {
+          home: "Homepage",
+          about: "About us"
+        }
+      },
+      fallbackLocale: "en"
+    },
+    vueI18nLoader: true,
+    vuex: {
+      moduleName: "i18n",
+      syncLocale: false,
+      syncMessages: false,
+      syncRouteParams: true
+    }
+  },
+
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Nuxt.js starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/api/configuration-css
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+
+  /*
+   ** HTTP module configuration
+   */
+  http: {
+    // See https://http.nuxtjs.org/api/#options
+  },
+
+  /*
+   ** Build configuration
+   ** Doc: https://nuxtjs.org/api/configuration-build
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <section>
+    <div>
+      <h1 class="title">nuxt-i18n</h1>
+      <div class="links">
+        <div>Switch locale:
+          <nuxt-link
+            class="button--green"
+            v-for="(locale, index) in localesExcludingCurrent"
+            :key="index"
+            :exact="true"
+            :to="switchLocalePath(locale.code)"
+          >{{ locale.name }}</nuxt-link>
+        </div>
+      </div>
+      <div class="links">Navigate:
+        <nuxt-link :to="localePath('about')" class="button--green">{{ $t('about') }}</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    localesExcludingCurrent() {
+      return this.$i18n.locales.filter(
+        locale => locale.code !== this.$i18n.locale
+      );
+    }
+  }
+};
+</script>
+
+<script>
+export default {
+  computed: {
+    localesExcludingCurrent() {
+      return this.$i18n.locales.filter(
+        locale => locale.code !== this.$i18n.locale
+      );
+    }
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
